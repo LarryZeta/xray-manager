af029d2348b4f985fd36ee6d8487a4c37ff9cd58
LarryZeta <i@larryzeta.cc>
2019-07-31 16:49:44 +0800

    start project
    Changes to be committed:
    	modified:   .gitignore
    	new file:   Billing-System.iml
    	new file:   pom.xml
    	new file:   src/main/java/cc/larryzeta/bill/BillApplication.java
    	new file:   src/main/resources/application.properties
    	new file:   src/main/resources/templates/login.html
    	new file:   src/main/resources/templates/register.html
    	new file:   src/test/java/cc/larryzeta/bill/BillApplicationTests.java

---
+++ src/main/java/cc/larryzeta/bill/BillApplication.java
@@ -0,0 +1,13 @@
+package cc.larryzeta.bill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillApplication.class, args);
+    }
+
+}
