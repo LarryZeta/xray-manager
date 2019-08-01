1646ce5dd488bf5f8e238d23e24ac277e75a48f5
LarryZeta <i@larryzeta.cc>
2019-08-01 17:01:51 +0800

    完善登录功能 Changes to be committed: 	modified:   src/main/java/cc/larryzeta/bill/config/MvcConfigure.java 	modified:   src/main/java/cc/larryzeta/bill/controller/LoginController.java 	modified:   src/main/resources/templates/login.html

--- src/main/java/cc/larryzeta/bill/config/MvcConfigure.java
+++ src/main/java/cc/larryzeta/bill/config/MvcConfigure.java
@@ -9,6 +9,7 @@ public class MvcConfigure implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/login.html").setViewName("login");
         registry.addViewController("/register").setViewName("register");
--- src/main/java/cc/larryzeta/bill/controller/LoginController.java
+++ src/main/java/cc/larryzeta/bill/controller/LoginController.java
@@ -1,14 +1,29 @@
 package cc.larryzeta.bill.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Controller
 public class LoginController {
 
-//    @RequestMapping("/")
-//    public String login() {
-//        return "login";
-//    }
+    @PostMapping(value = "/user/login")
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        Map<String, Object> map, HttpSession session) {
+        if (!StringUtils.isEmpty(username) && "123".equals(password)) {
+            session.setAttribute("loginUser", username);
+            return "redirect:index.html";
+        } else {
+            map.put("msg", "Invalid username or password");
+            return "login";
+        }
+    }
 }
