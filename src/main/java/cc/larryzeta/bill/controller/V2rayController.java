c74e06d7bc5f57bf9e03794718df7099bd533d6a
LarryZeta <i@larryzeta.cc>
2019-08-16 18:41:24 +0800

    bugFix orders 404

--- src/main/java/cc/larryzeta/bill/controller/V2rayController.java
+++ src/main/java/cc/larryzeta/bill/controller/V2rayController.java
@@ -5,15 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class V2rayController {
 
     @Autowired
     V2rayService v2rayService;
 
-    @GetMapping(value = "/order")
+    @GetMapping(value = "/orders")
     public String getClients(Model model) {
         v2rayService.getAllClients(model);
-        return "order";
+        return "orders";
     }
 
 }
