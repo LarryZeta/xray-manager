94b540ab0e4b388e7715a972ab325be3b0c773ae
LarryZeta <i@larryzeta.cc>
2022-03-22 18:01:59 +0800

    add sync api

--- xray-shim/api_service.py
+++ xray-shim/api_service.py
@@ -37,9 +37,9 @@ def build_client(email, id, protocol):
 
 
 # 添加新账号功能
-def add_new_account(id, email):
+def add_client(id, email):
 
-    logging.info('[service-add_new_account] email:[%s], id:[%s]', email, id)
+    logging.info('[service-add_client] email:[%s], id:[%s]', email, id)
 
     xray_config = load_config()
     inbounds = xray_config['inbounds']
@@ -50,7 +50,7 @@ def add_new_account(id, email):
         # email相同的 直接跳过
         for client in clients:
             if client.get('email') is not None and email == client['email']:
-                logging.warn('[service-add_new_account] [%s]- [%s] 邮箱账号已存在，结束', protocol, email)
+                logging.warn('[service-add_client] [%s]- [%s] 邮箱账号已存在，结束', protocol, email)
                 return
         client = build_client(email, id, protocol)
         clients.append(client)
@@ -62,22 +62,43 @@ def add_new_account(id, email):
 
 
 # 删除账号功能
-def delete_account(email):
+def delete_client(email):
 
-    logging.info('[service-del_account] email:[%s]', email)
+    logging.info('[service-delete_client] email:[%s]', email)
 
     xray_config = load_config()
     inbounds = xray_config['inbounds']
     for inbound in inbounds:
         protocol = inbound['protocol']
-        logging.info('[service-del_account] protocol: [%s]', protocol)
+        logging.info('[service-delete_client] protocol: [%s]', protocol)
         clients = inbound['settings']['clients']
         # email查重删除
         clients = list(filter(lambda client : client.get('email') is None or email != client['email'], clients))
         inbound['settings']['clients'] = clients
 
-    logging.info('[service-del_account] 删除匹配到的账号')
+    logging.info('[service-delete_client] 删除匹配到的账号')
     save_config(xray_config)
     
     os.system('service xray restart')
 
+
+# 同步账号功能
+def sync_client(clients):
+    
+    logging.info('[service-sync_client] clients:[%s]', clients)
+
+    xray_config = load_config()
+    inbounds = xray_config['inbounds']
+    for inbound in inbounds:
+        protocol = inbound['protocol']
+        logging.info('[service-sync_client] protocol: [%s]', protocol)
+        update_clients = []
+        for account in clients:
+            update_client = build_client(account['email'], account['id'], protocol)
+            update_clients.append(update_client)
+        inbound['settings']['clients'] = update_clients
+
+    logging.info('[service-sync_client] 同步账号到文件')
+    save_config(xray_config)
+
+    os.system('service xray restart')
--- xray-shim/controller.py
+++ xray-shim/controller.py
@@ -4,13 +4,13 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/api/add', methods=['POST'])
-def add_new_account():
+def add_client():
     print(request.json)
     json = request.json
     id = json['id']
     email = json['email']
     try:
-        api_service.add_new_account(id, email)
+        api_service.add_client(id, email)
     except BaseException:
         return {
             'code': '9999',
@@ -23,12 +23,29 @@ def add_new_account():
 
 
 @app.route('/api/remove', methods=['POST'])
-def delete_account():
+def delete_client():
     print(request.json)
     json = request.json
     email = json['email']
     try:
-        api_service.delete_account(email)
+        api_service.delete_client(email)
+    except BaseException: 
+        return {
+            'code': '9999',
+            'msg': 'FAIL'
+        }
+    return {
+        'code': '0000',
+        'msg': 'SUCCESS'
+    }
+
+
+@app.route('/client/sync', methods=['POST', 'PUT'])
+def sync_client():
+    print(request.json)
+    clients = request.json['clients']
+    try:
+        api_service.sync_client(clients)
     except BaseException: 
         return {
             'code': '9999',
